1b934f43479b6e83e76285c1d203ba1744ecadad
Shane Doyle <shanedoyle1989@gmail.com>
2024-10-04 00:04:51 +0100

    feat: add additional props

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -10,41 +10,68 @@ const StarContainerStyle = {
   display: "flex",
 };
 
-const textStyle = {
-  lineHeight: "0",
-  margin: "",
-};
-
-export default function StarRating({ maxRating = 5 }) {
-  const [rating, setRating] = useState(0);
+export default function StarRating({
+  defaultRating = 0,
+  color = "#fcc419",
+  size = 24,
+  maxRating = 5,
+  messages = [],
+  className = "",
+  onSetRating,
+}) {
+  const [rating, setRating] = useState(defaultRating);
   const [tempRating, setTempRating] = useState(0);
 
+  function handleSetRating(rating) {
+    setRating(rating);
+    onSetRating(rating);
+  }
+
+  const textStyle = {
+    lineHeight: "0",
+    margin: "",
+    color,
+    fontSize: `${size}px`,
+  };
   return (
-    <div style={containerStyle}>
+    <div className={className} style={containerStyle}>
       <span style={StarContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
           <Star
             key={i}
+            color={color}
+            size={size}
             isFilled={tempRating ? tempRating >= i + 1 : rating >= i + 1}
-            onStarClick={() => setRating(i + 1)}
-            onStarHoverEnter={() => setTempRating(i + 1)}
-            onStarHoverLeave={() => setTempRating(0)}
+            onStarClick={() => handleSetRating(i + 1)}
+            onStarEnter={() => setTempRating(i + 1)}
+            onStarLeave={() => setTempRating(0)}
           />
         ))}
       </span>
-      <p style={textStyle}>{tempRating || rating || ""}</p>
+      <p style={textStyle}>
+        {messages.length === maxRating
+          ? messages[tempRating ? tempRating - 1 : rating - 1]
+          : tempRating || rating || ""}
+      </p>
     </div>
   );
 }
 
-const starStyle = {
-  display: "block",
-  width: "24px",
-  height: "24px",
-  cursor: "pointer",
-};
+function Star({
+  color,
+  size,
+  isFilled,
+  onStarClick,
+  onStarEnter,
+  onStarLeave,
+}) {
+  const starStyle = {
+    display: "block",
+    width: `${size}px`,
+    height: `${size}px`,
+    cursor: "pointer",
+  };
 
-function Star({ isFilled, onStarClick, onStarEnter, onStarLeave }) {
   return (
     <span
       role="button"
@@ -57,8 +84,8 @@ function Star({ isFilled, onStarClick, onStarEnter, onStarLeave }) {
         <svg
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 20 20"
-          fill="#333"
-          stroke="#333"
+          fill={color}
+          stroke={color}
         >
           <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
         </svg>
@@ -67,7 +94,7 @@ function Star({ isFilled, onStarClick, onStarEnter, onStarLeave }) {
           xmlns="http://www.w3.org/2000/svg"
           fill="none"
           viewBox="0 0 24 24"
-          stroke="#333"
+          stroke={color}
         >
           <path
             strokeLinecap="round"
