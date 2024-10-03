8729c3d9a852eb750b788adbc9f120558834582d
Shane Doyle <shanedoyle1989@gmail.com>
2024-10-03 19:34:28 +0100

    feat: add Star component

--- src/StarRating.jsx
+++ src/StarRating.jsx
@@ -8,7 +8,6 @@ const containerStyle = {
 
 const StarContainerStyle = {
   display: "flex",
-  gap: "4px",
 };
 
 const textStyle = {
@@ -18,14 +17,36 @@ const textStyle = {
 
 export default function StarRating({ maxRating = 5 }) {
   return (
-    <>
-      <div className={containerStyle}>
+    <div style={containerStyle}>
+      <span style={StarContainerStyle}>
         {Array.from({ length: maxRating }, (_, i) => (
-          <span className={StarContainerStyle}>S{i + 1}</span>
+          <Star key={i} />
         ))}
-      </div>
+      </span>
       <p style={textStyle}>10</p>
-    </>
+    </div>
+  );
+}
+
+const starStyle = {
+  display: "block",
+  width: "24px",
+  height: "24px",
+  cursor: "pointer",
+};
+
+function Star() {
+  return (
+    <span role="button" style={starStyle}>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 20 20"
+        fill="#333"
+        stroke="#333"
+      >
+        <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z" />
+      </svg>
+    </span>
   );
 }
 
