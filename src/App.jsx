b92ebc291cb501a3bb1cded2548618fdbe9ac8e0
Shane Doyle <shanedoyle1989@gmail.com>
2024-10-07 13:31:32 +0100

    feat: fetch movie and render movie data in MovieDetails component

--- src/App.jsx
+++ src/App.jsx
@@ -210,12 +210,59 @@ function ErrorMessage({ message }) {
 }
 
 function MovieDetails({ movieId, onMovieClose }) {
+  const [movie, setmovie] = useState({});
+
+  const {
+    Title: title,
+    Year: year,
+    Poster: poster,
+    Runtime: runtime,
+    imdbRating,
+    Plot: plot,
+    Released: released,
+    Actors: actors,
+    Director: director,
+    Genre: genre,
+  } = movie;
+
+  useEffect(() => {
+    async function getMovieDetails() {
+      const response = await fetch(
+        `http://www.omdbapi.com/?apikey=f18fa495&i=${movieId}`
+      );
+      const data = await response.json();
+      setmovie(data);
+    }
+    getMovieDetails();
+  }, []);
+
   return (
     <div className="details">
-      <button className="btn-back" onClick={onMovieClose}>
-        &larr;
-      </button>
-      {movieId}
+      <header>
+        <button className="btn-back" onClick={onMovieClose}>
+          &larr;
+        </button>
+        <img src={poster} alt="movie poster" />
+        <div className="details-overview">
+          <h2>{title}</h2>
+          <p>
+            {released} &bull; {runtime}
+          </p>
+          <p>{genre}</p>
+          <p>
+            <span>⭐️</span>
+            {imdbRating} IMDB Rating
+          </p>
+        </div>
+      </header>
+
+      <section>
+        <p>
+          <em>{plot}</em>
+        </p>
+        <p>Starring {actors}</p>
+        <p>Directed by {director}</p>
+      </section>
     </div>
   );
 }
